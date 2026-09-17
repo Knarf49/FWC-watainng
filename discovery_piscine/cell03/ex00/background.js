f5eda9f9e175eb636871cf286da8ace53cae3244
Frank <wasutha1449@gmail.com>
2026-09-17 17:08:34 +0700

    add padStart

--- discovery_piscine/cell03/ex00/background.js
+++ discovery_piscine/cell03/ex00/background.js
@@ -4,7 +4,7 @@ const getRandomHexColor = () => {
     "#" +
     Math.floor(Math.random() * 16777215)
       .toString(16)
-      .padStart(6, "0") 
+      .padStart(6, "0")
   );
 };
 const randomBtnHandler = () => {
